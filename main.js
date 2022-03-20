b3ab5cb45138e399336d500ce42f645d37a51ef5
Tony Xu <Design.TXU@gmail.com>
2022-03-21 10:49:05 +1100

    removed token, created config.json and .gitignore for token

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+
+client.once('ready', () => {
+    console.log('DesignBot Online!')
+})
+
+
+
+client.login('')
