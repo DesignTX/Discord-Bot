8270eb0c6ac79c9da8c789a0c245f9f8a002ed79
Tony Xu <Design.TXU@gmail.com>
2022-03-21 11:31:22 +1100

    added async and message embeds

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: 'reactionrole',
     description: "Sets up a reaction role message!",
-    execute(message, args, Discord, client) {
-        const channel = '955225281742913586'
+    async execute(message, args, Discord, client) {
+        const channel = '955232461133062167';
         const noGoodItemRole = message.guild.roles.cache.find(role => role.name === "No Good Items");
         const seriaRole = message.guild.roles.cache.find(role => role.name === "Seria");
         const legendaryRapportRole = message.guild.roles.cache.find(role => role.name === "Legendary Rapport");
@@ -12,6 +12,37 @@ module.exports = {
         const weiRole = message.guild.roles.cache.find(role => role.name === "Wei");
         const kaysarrRole = message.guild.roles.cache.find(role => role.name === "Kaysarr");
 
+        const noGoodItemEmoji = '❓';
+        const seriaEmoji = '🟦';
+        const legendaryRapportEmoji = '♥️';
+        const sianEmoji = '🟧';
+        const madnickEmoji = '🟪';
+        const mokamokaEmoji = '🟩';
+        const weiEmoji = '🟨';
+        const kaysarrEmoji = '🟥';
+
+        let embed = new Discord.MessageEmbed()
+        .setColor('#e42643')
+        .setTitle('ITEM ROLES')
+        .setDescription('Choose roles based on what items you would like to get pinged for!\n\n'
+            + `${sianEmoji} Sian Card\n`
+            + `${seriaEmoji} Seria Card\n`
+            + `${kaysarrEmoji} Kaysarr Card\n`
+            + `${weiEmoji} Wei Card\n`
+            + `${madnickEmoji} Madnick Card\n`
+            + `${legendaryRapportEmoji} Legendary Rapport\n`
+            + `${mokamokaEmoji} Mokamoka Card\n`
+            + `${noGoodItemEmoji} All Other Votes\n`
+        );
         
+        let messageEmbed = await message.channel.send(embed);
+        messageEmbed.react(sianEmoji);
+        messageEmbed.react(seriaEmoji);
+        messageEmbed.react(kaysarrEmoji);
+        messageEmbed.react(weiEmoji);
+        messageEmbed.react(madnickEmoji);
+        messageEmbed.react(legendaryRapportEmoji);
+        messageEmbed.react(mokamokaEmoji);
+        messageEmbed.react(noGoodItemEmoji);
     }
 }
