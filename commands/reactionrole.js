c0cfc3cd3b7f5a9470cc52e3a4d708f637d67398
Tony Xu <Design.TXU@gmail.com>
2022-03-21 11:06:17 +1100

    renaming roles

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -3,13 +3,15 @@ module.exports = {
     description: "Sets up a reaction role message!",
     execute(message, args, Discord, client) {
         const channel = '955225281742913586'
-        const noGoodItem = message.guild.roles.cache.find(role => role.name === "No Good Items");
-        const seria = message.guild.roles.cache.find(role => role.name === "Seria");
-        const legendaryRapport = message.guild.roles.cache.find(role => role.name === "Legendary Rapport");
-        const sian = message.guild.roles.cache.find(role => role.name === "Sian");
-        const madnick = message.guild.roles.cache.find(role => role.name === "Madnick");
-        const mokamoka = message.guild.roles.cache.find(role => role.name === "Mokamoka");
-        const wei = message.guild.roles.cache.find(role => role.name === "Wei");
-        const kaysarr = message.guild.roles.cache.find(role => role.name === "Kaysarr");
+        const noGoodItemRole = message.guild.roles.cache.find(role => role.name === "No Good Items");
+        const seriaRole = message.guild.roles.cache.find(role => role.name === "Seria");
+        const legendaryRapportRole = message.guild.roles.cache.find(role => role.name === "Legendary Rapport");
+        const sianRole = message.guild.roles.cache.find(role => role.name === "Sian");
+        const madnickRole = message.guild.roles.cache.find(role => role.name === "Madnick");
+        const mokamokaRole = message.guild.roles.cache.find(role => role.name === "Mokamoka");
+        const weiRole = message.guild.roles.cache.find(role => role.name === "Wei");
+        const kaysarrRole = message.guild.roles.cache.find(role => role.name === "Kaysarr");
+
+        
     }
 }
