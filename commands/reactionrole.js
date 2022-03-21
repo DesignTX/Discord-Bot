a6d2e005dd5affdb86d806ceee9782134154a159
Tony Xu <Design.TXU@gmail.com>
2022-03-21 12:34:54 +1100

    can now remove reaction roles

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -80,5 +80,41 @@ module.exports = {
             }
             
         });
+
+        client.on('messageReactionRemove', async (reaction, user) => {
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+
+            if (reaction.message.channel.id == channel) {
+                if(reaction.emoji.name === sianEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(sianRole);
+                }
+                if(reaction.emoji.name === seriaEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(seriaRole);
+                }
+                if(reaction.emoji.name === kaysarrEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(kaysarrRole);
+                }
+                if(reaction.emoji.name === weiEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(weiRole);
+                }
+                if(reaction.emoji.name === madnickEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(madnickRole);
+                }
+                if(reaction.emoji.name === legendaryRapportEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(legendaryRapportRole);
+                }
+                if(reaction.emoji.name === mokamokaEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(mokamokaRole);
+                }
+                if(reaction.emoji.name === noGoodItemEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(noGoodItemRole);
+                }
+            } else {
+                return;
+            }
+        })
     }
 }
